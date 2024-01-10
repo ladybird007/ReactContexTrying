414328ee0c71317afccf97d806230f06d5567175
ishunter216 <michael.king@edenprogram.com>
2024-01-10 14:49:07 +0200

    karkas of app

--- resto/src/index.js
+++ resto/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry';
+import RestoService from './services/resto-service';
+import RestoServiceContex from './components/resto-service-context';
+import store from './store';
 
 import './index.scss';
 
+const restoService = new RestoService();
 
-ReactDOM.render(
-    <App/>
-    , document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <RestoServiceContex.Provider value={restoService}>
+        <Router>
+          <App/>
+        </Router>
+      </RestoServiceContex.Provider>
+    </ErrorBoundry>
+  </Provider>
+)
 
---
+++ resto/src/reducers/index.js
@@ -0,0 +1,12 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'MENU_LOADED':
+      return {
+        menu: action.payload
+      };
+    default: 
+    return state;
+  }
+}
+
+export default reducer;
