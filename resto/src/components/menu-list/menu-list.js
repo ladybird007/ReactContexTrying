cd47c5816c75b652065f58edcd7bea8204113fdf
gorodnichaya-av <test@m.com>
2024-02-05 16:08:52 +0200

    added menu requesrted

--- resto/src/components/menu-list/menu-list.js
+++ resto/src/components/menu-list/menu-list.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import {connect} from 'react-redux';
 import WithRestoServise from '../hoc';
-import {menuLoaded} from '../../actions';
+import {menuLoaded, menuRequested} from '../../actions';
+import Spinner from '../spinner';
  
 import './menu-list.scss';
 
 class MenuList extends Component {
   componentDidMount() {
+    this.props.menuRequested();
+
     const {RestoService} = this.props;
     RestoService.getMenuItems()
     .then(res => this.props.menuLoaded(res));
@@ -15,7 +18,11 @@ class MenuList extends Component {
 
 
   render() {
-    const {menuItems} = this.props;
+    const {menuItems, loading} = this.props;
+
+    if (loading) {
+      return <Spinner/>
+    }
 
     return (
       <ul className="menu__list">
@@ -31,12 +38,14 @@ class MenuList extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    menuItems: state.menu
+    menuItems: state.menu,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = {
-  menuLoaded
+  menuLoaded,
+  menuRequested
 }
 
 export default WithRestoServise()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
--- resto/src/reducers/index.js
+++ resto/src/reducers/index.js
@@ -1,12 +1,24 @@
 const initialState = {
-  menu: []
+  menu: [],
+  loading: true
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'MENU_REQUESTED':
+      return {
+        menu: state.menu,
+        loading: true
+      };
     case 'MENU_LOADED':
       return {
-        menu: action.payload
+        menu: action.payload,
+        loading: false
+      };
+    case 'MENU_ERROR':
+      return {
+        menu: state.menu,
+        loading: true
       };
     default: 
       return state;
