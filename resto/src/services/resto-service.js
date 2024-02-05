0a38f8da70e91b17ec8a4f1c9e4d285b1c5cfd6c
ishunter216 <michael.king@edenprogram.com>
2024-02-05 15:35:32 +0200

    added services functions

--- resto/src/services/resto-service.js
+++ resto/src/services/resto-service.js
@@ -1,5 +1,16 @@
 export default class RestoService {
-  getMenuItems() {
-    return [];
+  _apiBase = 'http://localhost:3000';
+
+  async getResourse(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if(!res.ok) {
+      throw new Error(`Could net fetch ${url}` + `, received ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  async getMenuItems () {
+    return await this.getResourse(`/menu/`);
   }
 }
